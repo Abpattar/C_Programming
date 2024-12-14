b27f9be86cf63c31a452134c07439726bd6996c8
Abpattar <adityabpattar@gmail.com>
2024-12-14 22:44:35 +0530

    Deleted Simple_Intrest.exe

--- Simple_Intrest.c
+++ Simple_Intrest.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    float p, r, t, si;
+    float p, r, t;
     printf ("Enter the value of p, t, r\n");
     scanf ("%f %f %f", &p, &t, &r);
-    si = p*t*r/100;
+    float si = p*t*r/100;
     printf ("Simple Intrest = %.2f\n", si);
     return 0;
 }
