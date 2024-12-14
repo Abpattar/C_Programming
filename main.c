291de10e3b5bebf9ab62daabe85788bbf4f461d0
Abpattar <adityabpattar@gmail.com>
2024-12-14 22:19:38 +0530

    new

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float p, r, t, si;
+    printf ("Enter the value of p, t, r\n");
+    scanf ("%f %f %f", &p, &t, &r);
+    si = p*t*r/100;
+    printf ("Simple Intrest = %f\n", si);
+    return 0;
+}
