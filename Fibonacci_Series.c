e0c20e5013c11e186304a14c727680a55b5bf03f
Abpattar <adityabpattar@gmail.com>
2024-12-15 11:10:45 +0530

    Adding value if n = 0 or 1

--- Fibonacci_Series.c
+++ Fibonacci_Series.c
@@ -5,13 +5,20 @@ int main()
     printf ("Enter the value of n\n");
     scanf ("%d", &n);
     int a = 0, b = 1;
-    printf ("%d %d",a ,b);
-    for (int i = 2; i < n; i++)
+    if (n <= 1)
     {
+        printf ("%d", a);
+    }
+    else
+    {
+        printf ("%d %d",a ,b);
+        for (int i = 2; i < n; i++)
+        {
         int c = a + b;
         printf (" %d", c);
         a = b;
         b = c;
+        }
     }
     return 0;
 }
