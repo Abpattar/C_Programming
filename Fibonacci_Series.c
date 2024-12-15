8b9f387ca11d6b02ac5e2637d6f24fd5708b34ef
Abpattar <adityabpattar@gmail.com>
2024-12-15 11:02:43 +0530

    Fibonacci_Series program in C

---
+++ Fibonacci_Series.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf ("Enter the value of n\n");
+    scanf ("%d", &n);
+    int a = 0, b = 1;
+    printf ("%d %d",a ,b);
+    for (int i = 2; i < n; i++)
+    {
+        int c = a + b;
+        printf (" %d", c);
+        a = b;
+        b = c;
+    }
+    return 0;
+}
